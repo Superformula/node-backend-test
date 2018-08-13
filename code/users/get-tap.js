21a8806de9fd51f2cc9eb8ddb148262f14ed17c0
Peter Lyons <pete@peterlyons.com>
2018-08-13 09:31:00 -0600

    Add tests for GET user

---
+++ code/users/get-tap.js
@@ -0,0 +1,32 @@
+const request = require("supertest");
+const tap = require("tap");
+const uuid = require("uuid/v4");
+
+let uri;
+
+tap.beforeEach(async () => {
+  uri = await require("../get-test-uri")(require("../server"));
+});
+
+tap.test("get user base case", async test => {
+  const user = { id: uuid(), name: "Test User Name" };
+  await request(uri)
+    .post("/api/v1/users")
+    .send(user);
+  const res = await request(uri)
+    .get(`/api/v1/users/${user.id}`)
+    .expect(200);
+  test.match(res.body, user);
+});
+
+tap.test("get user not found case", async () => {
+  await request(uri)
+    .get(`/api/v1/users/${uuid()}`)
+    .expect(404);
+});
+
+tap.test("get user invalid userId case", async () => {
+  await request(uri)
+    .get(`/api/v1/users/${uuid()}FFF`)
+    .expect(400);
+});

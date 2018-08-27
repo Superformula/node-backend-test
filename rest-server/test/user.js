08d0c550abcfe4680b8f5194b3ccbf7375947d40
Patrick <pkwu@berkeley.edu>
2018-08-27 01:35:39 -0700

    Adds more tests and testing script

--- rest-server/test/user.js
+++ rest-server/test/user.js
@@ -32,8 +32,47 @@ describe('Users', () => {
           done();
         });
     });
+    
+    it('should list a SINGLE user on /users/:id GET', done => {
+      let newUser = new User({
+        name: 'Test User',
+        dob: '08/27/2018',
+        address: 'Los Angeles',
+        description: 'This is a test'
+      });
+      newUser.save( (err, data) => {
+        chai.request(server)
+          .get(`/api/users/${data.id}`)
+          .end( (err, res) => {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('_id');
+            res.body.should.have.property('name');
+            res.body.should.have.property('dob');
+            res.body.should.have.property('address');
+            res.body.should.have.property('description');
+            res.body.name.should.equal('Test User');
+            res.body.dob.should.equal('08/27/2018');
+            res.body.address.should.equal('Los Angeles');
+            res.body.description.should.equal('This is a test');
+            res.body._id.should.equal(data.id);
+            done();
+          });
+      });
+    });
 
-
+    it('should NOT fetch a non-existent user on /users/:id GET', done => {
+      chai.request(server)
+        .get('/api/users/1234567890')
+        .end( (err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.should.be.a('object');
+          res.should.have.property('error');
+          done();
+        });
+    });
   });
 
   /*
@@ -45,7 +84,7 @@ describe('Users', () => {
         name: 'Test User',
         dob: '08/27/2018',
         address: 'Los Angeles',
-        description: 'This is a test',
+        description: 'This is a test'
       };
       chai.request(server)
         .post('/api/users')
@@ -67,7 +106,23 @@ describe('Users', () => {
         });
     });
 
-
+    it('should NOT add a user on /users POST while missing one or more fields', done => {
+      let user = {
+        name: 'Test User',
+        dob: '08/27/2018',
+        address: 'Los Angeles'
+      };
+      chai.request(server)
+        .post('/api/users')
+        .send(user)
+        .end( (err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('error');
+          done();
+        });
+    });
   });
 
   /*
@@ -85,7 +140,7 @@ describe('Users', () => {
         name: 'Test User',
         dob: '08/27/2018',
         address: 'Los Angeles',
-        description: 'This is a test',
+        description: 'This is a test'
       });
       newUser.save( (err, data) => {
         chai.request(server)
@@ -95,6 +150,52 @@ describe('Users', () => {
             res.should.have.status(201);
             res.should.be.json;
             res.body.should.be.a('object');
+            done();
+          });
+      });
+    });
+
+    it('should update a user on /users/:id PUT with one or more fields missing', done => {
+      let updatedUser = {
+        name: 'Updated Name',
+        description: 'This is an update'
+      };
+      let newUser = new User({
+        name: 'Test User',
+        dob: '08/27/2018',
+        address: 'Los Angeles',
+        description: 'This is a test'
+      });
+      newUser.save( (err, data) => {
+        chai.request(server)
+          .put(`/api/users/${data.id}`)
+          .send(updatedUser)
+          .end( (err, res) => {
+            res.should.have.status(201);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            done();
+          });
+      });
+    });
+
+    it('should NOT update any users on /users/ PUT without specified param', done => {
+      let updatedUser = {
+        name: 'Updated Name',
+        description: 'This is an update'
+      };
+      let newUser = new User({
+        name: 'Test User',
+        dob: '08/27/2018',
+        address: 'Los Angeles',
+        description: 'This is a test'
+      });
+      newUser.save( (err, data) => {
+        chai.request(server)
+          .put('/api/users/')
+          .send(updatedUser)
+          .end( (err, res) => {
+            res.should.have.status(404);
             done();
           });
       });
@@ -110,7 +211,7 @@ describe('Users', () => {
         name: 'Test User',
         dob: '08/27/2018',
         address: 'Los Angeles',
-        description: 'This is a test',
+        description: 'This is a test'
       });
       newUser.save( (err, data) => {
         chai.request(server)
@@ -119,6 +220,26 @@ describe('Users', () => {
             res.should.have.status(200);
             res.should.be.json;
             res.body.should.be.a('object');
+            done();
+          });
+      });
+    });
+
+    it('should NOT delete a user on /users/:id DELETE with invalid id provided', done => {
+      let newUser = new User ({
+        name: 'Test User',
+        dob: '08/27/2018',
+        address: 'Los Angeles',
+        description: 'This is a test'
+      });
+      newUser.save( (err, data) => {
+        chai.request(server)
+          .delete('/api/users/1234567890')
+          .end( (err, res) => {
+            res.should.have.status(400);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('error');
             done();
           });
       });

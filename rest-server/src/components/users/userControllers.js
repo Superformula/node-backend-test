b947f98855446408d0384fbbfb18cf8da5713cc1
Patrick <pkwu@berkeley.edu>
2018-08-27 02:26:24 -0700

    Adds passing functionality for /users PUT

--- rest-server/src/components/users/userControllers.js
+++ rest-server/src/components/users/userControllers.js
@@ -2,7 +2,8 @@ import { addUserHelper, fetchAllUserHelper, fetchSingleUserHelper, updateUserHel
 
 const addUser = async (req, res) => {
   try {
-    res.status(201).send();
+    const newUser = await addUserHelper(req.body);
+    res.status(201).send(newUser);
   }
   catch (err) {
     res.status(400).send({error: 'Could not create User'});

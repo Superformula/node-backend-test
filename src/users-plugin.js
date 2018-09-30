744d73dc33cbc0036b77d74bc308024f262ff797
at55455 <at55455@gmail.com>
2018-09-30 13:31:10 -0400

    committing to show stubbed/pseudo-coded comments (how I start all work)

--- src/users-plugin.js
+++ src/users-plugin.js
@@ -3,10 +3,35 @@
  * @description defines the routes available on the users resource
  * @type {Hapi Plugin Object}
  */
+const joi       = require('joi')
+const userModel = require('./user-model')
+
 module.exports = {
   name: 'userService',
   version: '1.0.0',
   async register(server, options) {
+
+    /**
+     * @param  {name}
+     * @return {Object: User} New user object
+     */
+    server.route({
+      method:'POST',
+      path:'/',
+      options: {
+        validate: {
+          payload: {
+            name: joi.string().min(1).max(100).required()
+          }
+        }
+      },
+      handler(request, h) {
+        // add guid
+        // add timestamps
+        // return new user
+        return request.payload
+      }
+    })
 
     // GET users
     server.route({
@@ -14,15 +39,6 @@ module.exports = {
       path:'/',
       handler(request, h) {
         return 'GET Users'
-      }
-    })
-
-    // POST user
-    server.route({
-      method:'POST',
-      path:'/',
-      handler(request, h) {
-        return 'Create User' + JSON.stringify(request.payload)
       }
     })
 

800dd89fa8b541f6cf39890f599d13c1ca2db2eb
Zak sesti <sestizac@gmail.com>
2019-10-07 20:21:13 -0400

    add delete and update functionality.  refactor the UserDataAccess layer to reuse the dynamodb put call

--- test/UsersApiIntegrationTest.ts
+++ test/UsersApiIntegrationTest.ts
@@ -4,9 +4,9 @@ import { User } from '../src/model/User';
 import { UserCreate } from '../src/model/UserCreate';
 
 const UUID_LENGTH: number = 36;
+const api: supertest.SuperTest<supertest.Test> = supertest('http://localhost:3000');
 
 describe('POST /users', () => {
-  const api: supertest.SuperTest<supertest.Test> = supertest('http://localhost:3000');
 
   test('creates user', async () => {
     const userCreate: UserCreate = createNewUser();
@@ -21,10 +21,10 @@ describe('POST /users', () => {
   });
 });
 
-describe('GET / users / { id }', () => {
-  const api: supertest.SuperTest<supertest.Test> = supertest('http://localhost:3000');
+describe('with an existing user', () => {
   let createdUser: User;
-  beforeAll(async () => {
+
+  beforeEach(async () => {
     const userCreate: UserCreate = createNewUser();
     createdUser = (await api.post('/users')
       .send(userCreate)
@@ -32,21 +32,62 @@ describe('GET / users / { id }', () => {
       .body;
   });
 
-  test('gets user', async () => {
-    const fetchedUser: User = (await api.get(`/users/${createdUser.id}`)
-      .send()
-      .expect(200))
-      .body;
-    expect(fetchedUser.id).toEqual(createdUser.id);
-    expect(fetchedUser.createdAt).toEqual(createdUser.createdAt);
-    expect(fetchedUser).toEqual(createdUser);
+  describe('GET /users/{id}', () => {
+    test('gets user', async () => {
+      const fetchedUser: User = (await api.get(`/users/${createdUser.id}`)
+        .send()
+        .expect(200))
+        .body;
+      expect(fetchedUser.id).toEqual(createdUser.id);
+      expect(fetchedUser.createdAt).toEqual(createdUser.createdAt);
+      expect(fetchedUser).toEqual(createdUser);
+    });
+
+    test('errors with bad id', async () => {
+      await api.get('/users/invalid')
+        .send()
+        .expect(404);
+    });
   });
 
-  test('receives status 404 with bad id', async () => {
-    await api.get('/users/invalid')
-      .send()
-      .expect(404);
+  describe('PUT /users/{id}', () => {
+    const newName: string = 'new name';
+
+    test('updates the user', async () => {
+      const updatedUser: User = (await api.put(`/users/${createdUser.id}`)
+        .send({ name: newName })
+        .expect(200))
+        .body;
+      expect(updatedUser.updatedAt).toBeDefined();
+      expect(updatedUser.name).toEqual(newName);
+      expect(createdUser.name).not.toEqual(newName);
+    });
+
+    test('errors with a bad id', async () => {
+      await api.put('/users/invalid')
+        .send({ name: newName })
+        .expect(404);
+    });
   });
+
+  describe('DELETE /users/{id}', () => {
+    test('deletes a user', async () => {
+      await api.delete(`/users/${createdUser.id}/`)
+        .send()
+        .expect(204);
+
+      await api.get(`/users/${createdUser.id}/`)
+        .send()
+        .expect(404);
+    });
+
+    test('deleting an invalid id provides a success response', async () => {
+      await api.delete('/users/invalid/')
+        .send()
+        .expect(204);
+    });
+  });
+
 });
 
 const createNewUser = (): UserCreate => {

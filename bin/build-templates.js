b1e7f74e418b0a706179ac8be74f25ec39781d2d
mmonkey <mmonkey@gmail.com>
2019-10-23 02:31:55 -0500

    Added optional API key parameter. Added optional CloudWatch alarms. Fixed issue with build-templates script.

--- bin/build-templates.js
+++ bin/build-templates.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
+const inputPath = path.resolve(__dirname, './../templates');
+const outputPath = path.resolve(__dirname, './../dist/templates');
 const readdir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
@@ -17,17 +19,17 @@ const replacements = {
  * Then rewrite all replacements with the appropriate value.
  * Save rewritten .template files to dist/templates directory.
  */
-readdir(path.resolve(__dirname, './../templates'))
+mkdir(outputPath)
+	.then(() => readdir(path.resolve(__dirname, './../templates')))
 	.then(files => files.filter(file => file.indexOf('.template') > -1))
 	.then(files => {
 		return Promise.all(files.map(file => {
-			const inputPath = path.resolve(__dirname, `./../templates/${file}`);
-			return readFile(inputPath, 'utf8').then(body => {
-				Object.keys(replacements).forEach(key => {
-					body = body.replace(new RegExp(key, 'g'), replacements[key]);
+			return readFile(`${inputPath}/${file}`, 'utf8')
+				.then(body => {
+					Object.keys(replacements).forEach(key => {
+						body = body.replace(new RegExp(key, 'g'), replacements[key]);
+					});
+					return writeFile(`${outputPath}/${file}`, body);
 				});
-				const outputPath = path.resolve(__dirname, './../dist/templates');
-				return mkdir(outputPath).then(() => writeFile(`${outputPath}/${file}`, body));
-			});
 		}));
 	});
